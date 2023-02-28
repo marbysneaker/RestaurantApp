57142f640fee20a9004eb4d961563c12f665f4bb
marcelino pangilinan <marbysneaker@gmail.com>
2023-02-28 02:44:38 -0800

    fixed gradle issue

--- app/src/main/java/com/example/restaurantdeveloper/MainActivity.java
+++ app/src/main/java/com/example/restaurantdeveloper/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.restaurantdeveloper;
-
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;

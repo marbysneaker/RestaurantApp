598583e946b268fe19bf22c9305d2691841708cb
marcelino pangilinan <marbysneaker@gmail.com>
2023-02-28 02:37:20 -0800

    finishing xml

--- app/src/main/java/com/example/restaurantdeveloper/MainActivity.java
+++ app/src/main/java/com/example/restaurantdeveloper/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.restaurantdeveloper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    CardView startercard;
+    CardView maincard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startercard = findViewById(R.id.card_view_starters);
+        maincard = findViewById(R.id.card_view_main);
     }
 }

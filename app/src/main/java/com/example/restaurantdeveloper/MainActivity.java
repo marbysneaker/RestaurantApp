7b271a09255caead77915d8929dc1e4c40c4ddc6
marcelino pangilinan <marbysneaker@gmail.com>
2023-02-28 23:24:47 -0800

    finished starter menu

--- app/src/main/java/com/example/restaurantdeveloper/MainActivity.java
+++ app/src/main/java/com/example/restaurantdeveloper/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.restaurantdeveloper;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.cardview.widget.CardView;
 
@@ -15,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startercard = findViewById(R.id.card_view_starters);
         maincard = findViewById(R.id.card_view_main);
+
+        startercard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class );
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
